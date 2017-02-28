c83d06688b609b2e965548a393bfefcb8ddc0cc4
LinnikOleg <OLinnyk@luxoft.com>
2017-02-28 12:35:06 +0200

    updated package naming and added books

---
+++ src/main/java/lc/algorithms/easy/array/TwoSum.java
@@ -0,0 +1,36 @@
+package lc.algorithms.easy.array;
+
+/**
+ * Created by LinnikOleg on 16.01.17.
+ */
+public class TwoSum {
+
+	/**
+	 * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+     *
+	 * You may assume that each input would have exactly one solution.
+	 *
+	 * Example:
+	 * Given nums = [2, 7, 11, 15], target = 9,
+     *
+	 * cBecause nums[0] + nums[1] = 2 + 7 = 9,
+	 * return [0, 1].
+	 *
+	 * @param nums inputs array
+	 * @param target specific target
+	 * @return      position of sum result
+	 */
+	public int[] twoSum(int[] nums, int target) {
+		int[] result = new int[2];
+		for(int i = 0; i < nums.length; i++){
+			for(int j = i + 1; j < nums.length; j++){
+				if(target == nums[i] + nums[j]){
+					result[0] = i;
+					result[1] = j;
+					return result;
+				}
+			}
+		}
+		return result;
+	}
+}
