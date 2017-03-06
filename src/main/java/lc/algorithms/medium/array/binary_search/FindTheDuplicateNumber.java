03e8a69dcc21426e5a93aa32e42cfaf9fdf139f7
LinnykOleh <OLinnyk@luxoft.com>
2017-03-06 16:53:46 +0200

    FindTheDuplicateNumber_Best implementation but first have to get more knowledge

--- src/main/java/lc/algorithms/medium/array/binary_search/FindTheDuplicateNumber.java
+++ src/main/java/lc/algorithms/medium/array/binary_search/FindTheDuplicateNumber.java
@@ -49,3 +49,29 @@ public class FindTheDuplicateNumber {
         System.out.println(new FindTheDuplicateNumber().findDuplicate(new int[]{1, 2, 3, 4, 5, 8, 11, 11}));
     }
 }
+
+class FindTheDuplicateNumber_Best{
+
+
+    public int findDuplicate(int[] nums) {
+        int n = nums.length;
+        int slow = n;
+        int fast = n;
+
+        do{
+            slow = nums[slow-1];
+            fast = nums[nums[fast-1]-1];
+        }while(slow != fast);
+        slow = n;
+        while(slow != fast){
+            slow = nums[slow-1];
+            fast = nums[fast-1];
+        }
+        return slow;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new FindTheDuplicateNumber_Best().findDuplicate(new int[] {2, 5, 1, 1, 4, 3}));
+    }
+
+}
