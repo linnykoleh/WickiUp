f962245501ca1ce2b1b7ce4477f3aafc08d7342f
LinnykOleh <linnik.oleg.93@gmail.com>
2017-03-07 08:05:52 +0200

    Search2DMatrix added javadoc

--- src/main/java/lc/algorithms/medium/array/binary_search/Search2DMatrix.java
+++ src/main/java/lc/algorithms/medium/array/binary_search/Search2DMatrix.java
@@ -5,6 +5,26 @@ package lc.algorithms.medium.array.binary_search;
  */
 public class Search2DMatrix {
 
+    /**
+     * Write an efficient algorithm that searches for a value in an m x n matrix. This matrix has the following properties:
+     *
+     * Integers in each row are sorted from left to right.
+     * The first integer of each row is greater than the last integer of the previous row.
+     * For example,
+     *
+     * Consider the following matrix:
+     *
+     *   [
+     *     [1,   3,  5,  7],
+     *     [10, 11, 16, 20],
+     *     [23, 30, 34, 50]
+     *   ]
+     * Given target = 3, return true.
+     *
+     * @param matrix given matrix
+     * @param target target to find
+     * @return        is exist target in matrix
+     */
     public boolean searchMatrix(int[][] matrix, int target) {
         if(matrix.length > 0 && matrix[0].length > 0) {
             int rowIndex = findRow(matrix, target);
