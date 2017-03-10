539d19f1ff0706f2cf1aab014210bb1f182b99b1
LinnykOleh <linnik.oleg.93@gmail.com>
2017-03-10 07:01:04 +0200

    finished with DoublyLinkedList

---
+++ src/main/java/lafore/data_structures/linked_lists/doubly_linked_list/DoublyLinkedApp.java
@@ -0,0 +1,34 @@
+package lafore.data_structures.linked_lists.doubly_linked_list;
+
+/**
+ * @author LinnykOleh
+ */
+public class DoublyLinkedApp {
+
+    public static void main(String[] args) {
+
+        DoublyLinkedList doublyLinkedList = new DoublyLinkedList();
+
+        doublyLinkedList.insertFirst(22);
+        doublyLinkedList.insertFirst(44);
+        doublyLinkedList.insertFirst(66);
+
+        doublyLinkedList.insertLast(11);
+        doublyLinkedList.insertLast(33);
+        doublyLinkedList.insertLast(55);
+
+        doublyLinkedList.displayForward();
+        doublyLinkedList.displayBackward();
+
+        doublyLinkedList.deleteFirst();
+        doublyLinkedList.deleteLast();
+        doublyLinkedList.deleteKey(11);
+
+        doublyLinkedList.displayForward();
+
+        doublyLinkedList.insertAfter(22, 77);
+        doublyLinkedList.insertAfter(33, 88);
+
+        doublyLinkedList.displayForward();
+    }
+}
--- src/main/java/lafore/data_structures/linked_lists/doubly_linked_list/DoublyLinkedList.java
+++ src/main/java/lafore/data_structures/linked_lists/doubly_linked_list/DoublyLinkedList.java
@@ -26,15 +26,95 @@ public class DoublyLinkedList {
 	public void insertLast(int iData){
 		final Link newLink = new Link(iData);
 		if (isEmpty()) {
-			last = newLink;
+			first = newLink;
 		} else {
-			last.next= newLink;
+			last.next = newLink;
+			newLink.previous = last;
 		}
-		newLink.previous = last;
 		last = newLink;
 	}
 
-	public void insertAfter(int iData){
+	public boolean insertAfter(int key, int iData){
+		Link current = first;
+		while (current.iData != key) {
+			current = current.next;
+			if (current == null) {
+				return false;
+			}
+		}
+		Link newLink = new Link(iData);
+		if(current == last){
+			newLink.next = null;
+		}else{
+			newLink.next = current.next;
+			current.next.previous = newLink;
+		}
+		newLink.previous = current;
+		current.next = newLink;
+		return true;
+	}
 
+	public Link deleteFirst(){
+		Link tmp = first;
+		if(first.next == null){
+			last = null;
+		}else {
+			first.next.previous = null;
+		}
+		first = first.next;
+		return tmp;
+	}
+
+	public Link deleteLast(){
+		Link tmp = last;
+		if(first.next == null){
+			first = null;
+		}else{
+			last.previous.next = null;
+		}
+		last = last.previous;
+		return tmp;
+	}
+
+	public Link deleteKey(int iData){
+		Link current = first;
+		while (current.iData != iData){
+			current = current.next;
+			if(current == null){
+				return null;
+			}
+		}
+		if(current == first){
+			first = current.next;
+		}else {
+			current.previous.next = current.next;
+		}
+
+		if(current == last){
+			last = current.previous;
+		}else {
+			current.next.previous = current.previous;
+		}
+		return current;
+	}
+
+	public void displayForward(){
+		System.out.println("List (first --> Last) :");
+		Link current = first;
+		while (current != null){
+			current.displayLink();
+			current = current.next;
+		}
+		System.out.println("");
+	}
+
+	public void displayBackward(){
+		System.out.println("List (last --> first) : ");
+		Link current = last;
+		while (current != null){
+			current.displayLink();
+			current = current.previous;
+		}
+		System.out.println("");
 	}
 }
