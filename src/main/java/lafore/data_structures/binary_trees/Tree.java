08fbddd6fe5f2562c19ee1e7b900a6598c0df5f0
LinnykOleh <linnik.oleg.93@gmail.com>
2017-04-12 09:58:23 +0300

    binary trees more interesting information

--- src/main/java/lafore/data_structures/binary_trees/Tree.java
+++ src/main/java/lafore/data_structures/binary_trees/Tree.java
@@ -7,10 +7,10 @@ public class Tree {
 
     private Node root;
 
-    public Node find(int key){
+    public Node find(int iData){
         Node current = root;
-        while (current.iData != key){
-            if(key < current.iData){
+        while (current.iData != iData){
+            if(iData < current.iData){
                 current = current.leftChild;
             }else{
                 current = current.rightChild;
@@ -22,8 +22,55 @@ public class Tree {
         return current;
     }
 
-    public void insert(int id, double dData){
+    public void insert(int iData, double dData){
+        final Node newNode = new Node(); // Создание нового узла
+        newNode.iData = iData;     // Вставка данных
+        newNode.dData = dData;
 
+        if(root == null) {        // Корневой узел не существует
+            root = newNode;
+        } else {
+            Node current = root;   // Начать с корневого узла
+            Node parent;
+
+            while(true) {          // (Внутренний выход из цикла)
+                parent = current;
+                if(iData < current.iData) {  // Двигаться налево?
+                    current = current.leftChild;
+                    if(current == null){     // Если достигнут конец цепочки вставить слева
+                        parent.leftChild = newNode;
+                        return;
+                    }
+
+                } else {                     // Или направо?
+                    current = current.rightChild;
+                    if(current == null) {    // Если достигнут конец цепочки вставить справа
+                        parent.rightChild = newNode;
+                        return;
+                    }
+                }
+            }
+        }
+    }
+
+    public Node miminum(){
+        Node prev = root;
+        Node current = root;
+        while (current != null){
+            current = prev;
+            current = current.leftChild;
+        }
+        return prev;
+    }
+
+    public Node maximum(){
+        Node prev = root;
+        Node current = root;
+        while (current != null){
+            current = prev;
+            current = current.rightChild;;
+        }
+        return prev;
     }
 
     public void delete(int id){
