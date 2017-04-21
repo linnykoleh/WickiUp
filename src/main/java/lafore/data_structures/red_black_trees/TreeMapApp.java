7c3e678b3a51305a14fa8626dd3f33f3fd64a3df
LinnykOleh <linnik.oleg.93@gmail.com>
2017-04-21 15:43:57 +0300

    added custom tree map

---
+++ src/main/java/lafore/data_structures/red_black_trees/TreeMapApp.java
@@ -0,0 +1,32 @@
+package lafore.data_structures.red_black_trees;
+
+import org.junit.Test;
+
+import lafore.data_structures.red_black_trees.for_tests.Key;
+
+public class TreeMapApp {
+
+	@Test
+	public void testSimpleObjects() {
+		final TreeMap<Integer, String> treeMap = new TreeMap<>();
+
+		treeMap.put(4, "hello 4");
+		treeMap.put(5, "hello 5");
+		treeMap.put(3, "hello 3");
+		treeMap.put(2, "hello 2");
+		treeMap.put(6, "hello 6");
+		treeMap.put(1, "hello 1");
+	}
+
+	@Test
+	public void testTrickyObjects() {
+		final TreeMap<Key, String> treeMap = new TreeMap<>();
+
+		treeMap.put(new Key(4), "hello 4");
+		treeMap.put(new Key(5), "hello 5");
+		treeMap.put(new Key(3), "hello 3");
+		treeMap.put(new Key(2), "hello 2");
+		treeMap.put(new Key(6), "hello 6");
+		treeMap.put(new Key(1), "hello 1");
+	}
+}
---
+++ src/main/java/lafore/data_structures/red_black_trees/for_tests/Key.java
@@ -0,0 +1,26 @@
+package lafore.data_structures.red_black_trees.for_tests;
+
+
+public class Key implements Comparable<Key>{
+
+	private static int MORE = 1;
+	private static int LESS = -1;
+	private static int SAME = 0;
+
+	private int index;
+
+	public Key(int index) {
+		this.index = index;
+	}
+
+	@Override
+	public int compareTo(Key o) {
+		if(index > o.index){
+			return MORE;
+		}else if(index < o.index){
+			return LESS;
+		} else {
+			return SAME;
+		}
+	}
+}
