10924f9b0ddb3545b595fcf9025824664de1a44a
LinnykOleh <linnik.oleg.93@gmail.com>
2017-04-27 14:45:17 +0300

    single responsibility principle

---
+++ src/main/java/solid/s/bad/Order.java
@@ -0,0 +1,57 @@
+package solid.s.bad;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author LinnykOleh
+ */
+public class Order {
+
+	public double calculateTotalSum(){
+		// some business logic
+		return 0.0;
+	}
+
+	public List<Object> getItems(){
+		// some business logic
+		return new ArrayList<>();
+	}
+
+	public List<Integer> getItemCount(){
+		// some business logic
+		return new ArrayList<>();
+	}
+
+	public void addItem(Object item){
+		// some business logic
+	}
+
+	public void deleteItem(Object item){
+		// some business logic
+	}
+
+	public void printOrder(){
+		// some business logic
+	}
+
+	public void showOrder(){
+		// some business logic
+	}
+
+	public void load(){
+		// some business logic
+	}
+
+	public void save(){
+		// some business logic
+	}
+
+	public void update(){
+		// some business logic
+	}
+
+	public void delete(){
+		// some business logic
+	}
+}
