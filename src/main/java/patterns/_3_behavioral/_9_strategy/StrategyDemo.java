c55128bd8eeda9908e3abacccf262dbb7ad5cd0d
LinnykOleh <linnik.oleg.93@gmail.com>
2017-07-17 18:05:23 +0300

    added some refactoring

--- src/main/java/patterns/_3_behavioral/_9_strategy/StrategyDemo.java
+++ src/main/java/patterns/_3_behavioral/_9_strategy/StrategyDemo.java
@@ -3,11 +3,15 @@ package patterns._3_behavioral._9_strategy;
 /**
  * @author LinnykOleh
  */
-public class Demo {
+public class StrategyDemo {
 
     public static void main(String[] args) {
-        final CreditCard amexCard = new CreditCard(new AmexStrategy());
+        final StrategyDemo strategyDemo = new StrategyDemo();
+        strategyDemo.useStrategies();
+    }
 
+    public void useStrategies(){
+        final CreditCard amexCard = new CreditCard(new AmexStrategy());
         amexCard.setNumber("379185883464283");
         amexCard.setDate("04/2020");
         amexCard.setCvv("123");
@@ -15,7 +19,6 @@ public class Demo {
         System.out.println("Is Amex valid: " + amexCard.isValid());
 
         final CreditCard amexCard2 = new CreditCard(new AmexStrategy());
-
         amexCard2.setNumber("37929943181404123");
         amexCard2.setDate("04/2020");
         amexCard2.setCvv("123");
@@ -23,13 +26,10 @@ public class Demo {
         System.out.println("Is Amex valid: " + amexCard2.isValid());
 
         final CreditCard visaCard = new CreditCard(new VisaStrategy());
-
         visaCard.setNumber("492994318140412343");
         visaCard.setDate("11/19");
         visaCard.setCvv("321");
 
         System.out.println("Is Visa valid: " + amexCard2.isValid());
-
-
     }
 }
