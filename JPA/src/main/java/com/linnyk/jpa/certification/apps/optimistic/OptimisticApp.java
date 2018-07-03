0e7bb28db0332e6c7c4c856f40c41bca9a9c246c
LinnykOleh <linnik.oleg.93@gmail.com>
2018-07-03 15:09:22 +0200

    finished optimistic lock exception demo

--- JPA/src/main/java/com/linnyk/jpa/certification/apps/optimistic/OptimisticApp.java
+++ JPA/src/main/java/com/linnyk/jpa/certification/apps/optimistic/OptimisticApp.java
@@ -1,17 +1,58 @@
 package com.linnyk.jpa.certification.apps.optimistic;
 
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 
 import org.junit.Test;
 
+import com.linnyk.jpa.certification.entities.optimistic.OptimisticEntity;
 import com.linnyk.jpa.safari.jpa_api.configuration.JPAFactoryBuilder;
 
 public class OptimisticApp {
 
 	@Test
-	public void test(){
+	public void test() throws InterruptedException {
 		final EntityManagerFactory entityManagerFactory = JPAFactoryBuilder.getEntityManagerFactory();
+		final EntityManager entityManager = entityManagerFactory.createEntityManager();
+		final EntityTransaction transaction = entityManager.getTransaction();
+		transaction.begin();
 
+		final OptimisticEntity optimisticEntity = new OptimisticEntity();
+		optimisticEntity.setName("optimisticEntity");
 
+		entityManager.persist(optimisticEntity);
+
+		transaction.commit();
+		entityManager.close();
+
+		final EntityManager entityManager1 = entityManagerFactory.createEntityManager();
+		final EntityTransaction transaction1 = entityManager1.getTransaction();
+		transaction1.begin();
+
+		final OptimisticEntity optimisticEntity1 = entityManager1.find(OptimisticEntity.class, optimisticEntity.getId());
+		optimisticEntity1.setName("NewOptimisticEntity");
+
+		new Thread(() -> {
+			//Update version in order to get OptimisticLockException
+			final EntityManager entityManager2 = entityManagerFactory.createEntityManager();
+			final EntityTransaction transaction2 = entityManager2.getTransaction();
+			transaction2.begin();
+
+			final OptimisticEntity optimisticEntity2 = entityManager2.find(OptimisticEntity.class, optimisticEntity.getId());
+			optimisticEntity2.setVersion(2);
+
+			transaction2.commit();
+			entityManager2.close();
+
+		}).start();
+
+		Thread.sleep(10000);
+
+		transaction1.commit();
+		entityManager1.close();
+		// javax.persistence.OptimisticLockException: Batch update returned unexpected row count from update [0]; actual row count: 0; expected: 1
+
+		entityManagerFactory.close();
 	}
 }
--- JPA/src/main/java/com/linnyk/jpa/certification/entities/optimistic/OptimisticEntity.java
+++ JPA/src/main/java/com/linnyk/jpa/certification/entities/optimistic/OptimisticEntity.java
@@ -1,7 +1,9 @@
 package com.linnyk.jpa.certification.entities.optimistic;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Version;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -16,8 +18,12 @@ import lombok.Setter;
 public class OptimisticEntity {
 
 	@Id
+	@GeneratedValue
 	private Long id;
 
 	private String name;
+
+	@Version
+	private int version;
 
 }
