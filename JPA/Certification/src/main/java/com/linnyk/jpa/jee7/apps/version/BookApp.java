da72c74c7690039ba3cd8847c5495ed064708b09
LinnykOleh <linnik.oleg.93@gmail.com>
2018-10-29 15:58:48 +0100

    test increment version after merge

--- JPA/Certification/src/main/java/com/linnyk/jpa/jee7/apps/version/BookApp.java
+++ JPA/Certification/src/main/java/com/linnyk/jpa/jee7/apps/version/BookApp.java
@@ -37,4 +37,33 @@ public class BookApp {
 		entityManagerFactory.close();
 	}
 
+	@Test
+	public void testUpdate(){
+		final EntityManagerFactory entityManagerFactory = JPAFactoryBuilder.getEntityManagerFactory();
+		final EntityManager entityManager = entityManagerFactory.createEntityManager();
+		final EntityTransaction transaction = entityManager.getTransaction();
+
+		final Book book = new Book("H2G2", 21f, "Лучшая IT-книга", "123-456", 321, false);
+
+		transaction.begin();
+		entityManager.persist(book);
+		transaction.commit();
+		// insert into BOOK_VERSION (id, description, illustrations, isbn, nbOfPage, price, title, version) values (null, ?, ?, ?, ?, ?, ?, ?)
+
+		System.out.println("Book version is : " + book.getVersion()); //1
+
+		final EntityTransaction transaction1 = entityManager.getTransaction();
+		transaction1.begin();
+		book.setPrice(111.0F);
+		final Book merge = entityManager.merge(book);
+		System.out.println(merge);
+
+		transaction1.commit();
+		// update BOOK_VERSION set description=?, illustrations=?, isbn=?, nbOfPage=?, price=?, title=?, version=? where id=? and version=?
+		System.out.println("Book version is : " + book.getVersion()); //2
+
+		entityManager.close();
+		entityManagerFactory.close();
+	}
+
 }
