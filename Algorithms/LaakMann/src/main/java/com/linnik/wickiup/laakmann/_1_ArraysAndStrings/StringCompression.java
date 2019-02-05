1fe478f2d58838d89dd76f8cebcb1e0b559f45da
LinnykOleh <linnik.oleg.93@gmail.com>
2019-02-05 14:15:35 +0100

    String Compression task done

---
+++ Algorithms/LaakMann/src/main/java/com/linnik/wickiup/laakmann/_1_ArraysAndStrings/StringCompression.java
@@ -0,0 +1,28 @@
+package com.linnik.wickiup.laakmann._1_ArraysAndStrings;
+
+/**
+ * =================================================================================================
+ * String Compression: Implement a method to perform basic string compression using the counts
+ * of repeated characters. For example, the string aabcccccaaa would become a2blc5a3. If the
+ * "compressed" string would not become smaller than the original string, your method should return
+ * the original string. You can assume the string has only uppercase and lowercase letters (a - z).
+ * ==================================================================================================
+ */
+public class StringCompression {
+
+	public String compress(String str) {
+		StringBuilder compressed = new StringBuilder();
+		int countConsecutive = 0;
+		for (int i = 0; i < str.length(); i++) {
+			countConsecutive++;
+
+			/* If next character is different than current, append this char to result.*/
+			if (i + 1 >= str.length() || str.charAt(i) != str.charAt(i + 1)) {
+				compressed.append(str.charAt(i));
+				compressed.append(countConsecutive);
+				countConsecutive = 0;
+			}
+		}
+		return compressed.length() < str.length() ? compressed.toString() : str;
+	}
+}
---
+++ Algorithms/LaakMann/src/test/java/com/linnik/wickiup/laakmann/_1_ArraysAndStrings/StringCompressionTest.java
@@ -0,0 +1,36 @@
+package com.linnik.wickiup.laakmann._1_ArraysAndStrings;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class StringCompressionTest {
+
+	private final StringCompression stringCompression = new StringCompression();
+
+	@Test
+	public void test() {
+		String expected = "a2b1c5a3";
+
+		String actual = stringCompression.compress("aabcccccaaa");
+
+		Assert.assertEquals(expected, actual);
+	}
+
+	@Test
+	public void test1() {
+		String expected = "bcdooo";
+
+		String actual = stringCompression.compress("bcdooo");
+
+		Assert.assertEquals(expected, actual);
+	}
+
+	@Test
+	public void test2() {
+		String expected = "b1c1d1o7";
+
+		String actual = stringCompression.compress("bcdooooooo");
+
+		Assert.assertEquals(expected, actual);
+	}
+}
